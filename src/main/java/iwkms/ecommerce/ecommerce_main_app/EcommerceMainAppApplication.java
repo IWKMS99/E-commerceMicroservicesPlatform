2955b28a9994354452617ef716db354df73c6230
IWKMS99 <alievsaid0909@yandex.ru>
2025-05-25 20:09:18 +0300

    Initial commit

---
+++ src/main/java/iwkms/ecommerce/ecommerce_main_app/EcommerceMainAppApplication.java
@@ -0,0 +1,13 @@
+package iwkms.ecommerce.ecommerce_main_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceMainAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceMainAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/iwkms/ecommerce/ecommerce_main_app/EcommerceMainAppApplicationTests.java
@@ -0,0 +1,13 @@
+package iwkms.ecommerce.ecommerce_main_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceMainAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
